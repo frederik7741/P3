9ae72cf98cc5e4724525bae8c78e679e24c7ba30
Bisgaard <mbisga22@student.aau.dk>
2023-10-26 13:00:54 +0200

    Fixed header and buttons
    Took 2,5 hours...

--- UI/P3UIUnity/Assets/Scripts/CanvasInteractivity.cs
+++ UI/P3UIUnity/Assets/Scripts/CanvasInteractivity.cs
@@ -5,26 +5,23 @@ using UnityEngine.SceneManagement;
 
 public class CanvasInteractivity : MonoBehaviour
 {
-    public GameObject backButtonPrefab; // Return Button prefab
-    public GameObject headerPrefab; // Header prefab
-    public RectTransform backgroundPanel; // The UI panel named "Background"
+    public GameObject headerPrefab;
+    public RectTransform backgroundPanel;
     public Vector2 returnButtonSize = new Vector2(100, 50);
     public GameObject buttonPrefab;
-    public RectTransform buttonParent;  // The grid for buttons inside the "Background" panel
-    public string[] buttonNames;  // Names set in the Unity Inspector
+    public RectTransform buttonParent;
+    public string[] buttonNames;
     public Vector2 buttonSize = new Vector2(100, 50);
     private Button øvelserButton;
     private Button dataButton;
 
     private void Awake()
     {
-        // Subscribe to the sceneLoaded event
         SceneManager.sceneLoaded += OnSceneLoaded;
     }
 
     private void OnDestroy()
     {
-        // Unsubscribe from the event
         SceneManager.sceneLoaded -= OnSceneLoaded;
     }
 
@@ -34,27 +31,34 @@ public class CanvasInteractivity : MonoBehaviour
         GridLayoutGroup grid = buttonParent.gameObject.AddComponent<GridLayoutGroup>();
         grid.cellSize = buttonSize;
         grid.spacing = new Vector2(10, 10);
-    
+
         // Adjust the top padding to move the buttons down
-        grid.padding.top = 150;  // Adjust this value as needed
+        grid.padding.top = 150;
 
         // Dynamically create name buttons
         for (int i = 0; i < buttonNames.Length; i++)
         {
             Button btn = Instantiate(buttonPrefab, buttonParent).GetComponent<Button>();
             TextMeshProUGUI buttonText = btn.GetComponentInChildren<TextMeshProUGUI>();
+
             if (buttonText != null)
             {
                 buttonText.text = buttonNames[i];
             }
-            else
-            {
-                Debug.LogError("Button does not have a TextMeshProUGUI component!");
-            }
+
             btn.onClick.AddListener(ShowOptions);
         }
 
-       
+// Instantiate "Øvelser" and "Data" buttons outside the loop, directly under the rootCanvas
+        Canvas rootCanvas = backgroundPanel.GetComponentInParent<Canvas>();
+        øvelserButton = Instantiate(buttonPrefab, rootCanvas.transform).GetComponent<Button>();
+        øvelserButton.GetComponentInChildren<TextMeshProUGUI>().text = "Øvelser";
+        øvelserButton.gameObject.SetActive(false); // Initially hide the button
+
+        dataButton = Instantiate(buttonPrefab, rootCanvas.transform).GetComponent<Button>();
+        dataButton.GetComponentInChildren<TextMeshProUGUI>().text = "Data";
+        dataButton.gameObject.SetActive(false); // Initially hide the button
+
     }
 
     private void OnSceneLoaded(Scene scene, LoadSceneMode mode)
@@ -67,20 +71,33 @@ public class CanvasInteractivity : MonoBehaviour
 
     private void InstantiateHeader()
     {
-        // Get the root canvas
         Canvas rootCanvas = backgroundPanel.GetComponentInParent<Canvas>();
-
-        // Instantiate the header under the canvas
         GameObject headerObj = Instantiate(headerPrefab, rootCanvas.transform);
     }
 
-
-
-
     private void ShowOptions()
     {
-        // Implementation for the ShowOptions method (unchanged)
+        if (øvelserButton != null && dataButton != null)
+        {
+            øvelserButton.gameObject.SetActive(true);
+            dataButton.gameObject.SetActive(true);
+
+            // Canvas size
+            Vector2 canvasSize = øvelserButton.GetComponentInParent<CanvasScaler>().referenceResolution;
+
+            // Position the "Øvelse" button to the bottom center-left
+            øvelserButton.GetComponent<RectTransform>().anchoredPosition = new Vector2(-buttonSize.x / 1, -canvasSize.y / 2 + buttonSize.y + 5);
+
+            // Position the "Data" button to the bottom center-right
+            dataButton.GetComponent<RectTransform>().anchoredPosition = new Vector2(buttonSize.x / 1, -canvasSize.y / 2 + buttonSize.y + 5);
+        }
     }
+
+
+
+
+
+
 
     private void LoadSceneByName(string sceneName)
     {
