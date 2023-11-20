d326654c1764c86fad04dff0d88f656918b237cd
Goodest person <mflens22@student.aau.dk>
2023-11-20 15:00:15 +0100

    Blobbers push

--- BackEnd/main.py
+++ BackEnd/main.py
@@ -15,11 +15,11 @@ if not zed.is_opened():
         exit()
 
 # Background subtraction parameters
-min_depth_threshold = 1000  # Minimum depth threshold for 1 meter (in millimeters)
-max_depth_threshold = 3000  # Maximum depth threshold for 2 meters (in millimeters)
+min_depth_threshold = 500  # Minimum depth threshold for 1 meter (in millimeters)
+max_depth_threshold = 2000  # Maximum depth threshold for 2 meters (in millimeters)
 
 min_contour_area = 100  # Adjust this threshold as needed
-max_contour_area = 200  # Adjust this threshold as needed
+#max_contour_area = 200  # Adjust this threshold as needed
 
 
 while True:
@@ -35,55 +35,93 @@ while True:
         depth_image = depth_data.get_data()
 
         # sets the Region Of Interest (ROI) to exclude the floor
-        Roi_width = 600
-        Roi_Floor = 130 + 100  # Exclude 130 pixels from the bottom and 100 pixels from the top
+        #Roi_width = 600
+        #Roi_Floor = 130 + 100  # Exclude 130 pixels from the bottom and 100 pixels from the top
 
-        New_Width = image.get_width() - Roi_width
-        New_Height = image.get_height() - Roi_Floor
+        #New_Width = image.get_width() - Roi_width
+        #New_Height = image.get_height() - Roi_Floor
 
         # Calculate the ROI to achieve the desired width and height
-        roi = (slice(New_Height), slice(New_Width))
-            #(image.get_width() - Roi_width) // 2, (image.get_width() + Roi_width) // 2))
+        #roi = (slice(New_Height), slice(New_Width))
 
         # Apply ROI to the images
-        image_roi = image.get_data()[roi]
-        depth_image_roi = depth_image[roi]
+        #image_roi = image.get_data()[roi]
+        #depth_image_roi = depth_image[roi]
 
         # Create a binary mask for foreground detection based on the depth range
-        foreground_mask = ((depth_image_roi >= min_depth_threshold) & (depth_image_roi <= max_depth_threshold)).astype('uint8')
+        #foreground_mask = ((depth_image_roi >= min_depth_threshold) & (depth_image_roi <= max_depth_threshold)).astype('uint8')
+
+
+        # Inside the loop, after retrieving the RGB image
+        color_image = image.get_data()
+        #color_image_roi = color_image[roi]
+        #hsv_image = cv2.cvtColor(color_image_roi, cv2.COLOR_BGR2HSV)
+        hsv_image = cv2.cvtColor(color_image, cv2.COLOR_BGR2HSV)
 
         # Apply morphological operations to refine the mask
         kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (3, 3))
         kernel2 = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (5, 5))
+        kernel3 = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (7,7))
 
-        foreground_mask = cv2.morphologyEx(foreground_mask, cv2.MORPH_ERODE, kernel2)
-
-
-
-
-        for _ in range(4):
-            foreground_mask = cv2.morphologyEx(foreground_mask, cv2.MORPH_CLOSE, kernel)
+        #foreground_mask = cv2.morphologyEx(foreground_mask, cv2.MORPH_ERODE, kernel2)
 
 
 
-        #eafjaef
-            # Inside the loop, after retrieving the RGB image
-        color_image = image.get_data()
-        hsv_image = cv2.cvtColor(color_image, cv2.COLOR_BGR2HSV)
+        """for _ in range(4):
+            foreground_mask = cv2.morphologyEx(foreground_mask, cv2.MORPH_CLOSE, kernel)"""
+
+
 
         yellow_centroids = []
 
         # Define the range for yellow color in HSV
-        lower_yellow = np.array([20, 100, 100], dtype=np.uint8)
-        upper_yellow = np.array([30, 255, 255], dtype=np.uint8)
+        lower_yellow = np.array([25, 130, 130])
+        upper_yellow = np.array([35, 210, 200])
 
             # Create a mask for yellow color
+
+
+
+        #yellow_mask = ((depth_image >= min_depth_threshold) & (depth_image <= max_depth_threshold)).astype('uint8')
         yellow_mask = cv2.inRange(hsv_image, lower_yellow, upper_yellow)
-        yellow_mask_roi = yellow_mask[roi]
+        cv2.imshow("Yellow mask before", yellow_mask)
+        #yellow_mask_roi = yellow_mask[roi]
 
-        combined_mask = cv2.bitwise_and(foreground_mask, yellow_mask_roi)
+       # yellow_mask_filter = cv2.morphologyEx(yellow_mask, cv2.MORPH_DILATE, kernel3)
+        #yellow_mask_filter_final = cv2.morphologyEx(yellow_mask_filter, cv2.MORPH_ELLIPSE, kernel3)
+        #yellow_mask = cv2.morphologyEx(yellow_mask, cv2.MORPH_ELLIPSE, kernel3)
+       # for _ in range(4):
+           # yellow_mask_filter2 = cv2.morphologyEx(yellow_mask, cv2.MORPH_DILATE, kernel3)
+        yellow_mask_filter = cv2.morphologyEx(yellow_mask, cv2.MORPH_CLOSE, kernel3)
 
-        yellow_contours, _ = cv2.findContours(combined_mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    #yellow_mask = cv2.morphologyEx(yellow_mask, cv2.MORPH_ELLIPSE, kernel3)
+       #or _ in range(4):
+       # yellow_mask_filter = cv2.morphologyEx(yellow_mask_filter2, cv2.MORPH_CLOSE, kernel3)
+
+        #yellow_mask = cv2.morphologyEx(yellow_mask_roi, cv2.MORPH_DILATE, kernel3)
+        #yellow_mask = cv2.morphologyEx(yellow_mask, cv2.MORPH_CLOSE, kernel)
+
+        #combined_mask = cv2.bitwise_and(foreground_mask, yellow_mask_roi)
+
+        depth_filtered_yellow_mask = np.logical_and(depth_image >= min_depth_threshold, depth_image <= max_depth_threshold)
+        yellow_mask = cv2.bitwise_and(yellow_mask_filter, yellow_mask_filter, mask=depth_filtered_yellow_mask.astype(np.uint8))
+
+        yellow_contours, _ = cv2.findContours(yellow_mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+
+        min_blob_area = 30
+        max_blob_area = 100
+
+        bloobers = []
+
+        for yellow_contour in yellow_contours:
+            area = cv2.contourArea(yellow_contour)
+            if min_blob_area <= area <= max_blob_area:
+                bloobers.append(yellow_contours)
+
+        pruned_mask = np.zeros_like(yellow_mask)
+        cv2.drawContours(pruned_mask, bloobers, -1, 255, thickness=cv2.FILLED)
+
 
         #limiting the size
         filtered_yellow_contours = [contour for contour in yellow_contours if cv2.contourArea(contour) > min_contour_area]
@@ -99,8 +137,6 @@ while True:
             centroid_y = y + h // 2
             yellow_centroids.append((centroid_x, centroid_y))
 
-
-
             # Connect yellow objects with lines
         for i in range(len(yellow_contours) - 1):
             if i < len(yellow_centroids) - 1:
@@ -108,9 +144,12 @@ while True:
                 x2, y2 = yellow_centroids[i + 1]
                 cv2.line(color_image, (x1, y1), (x2, y2), (255, 0, 0), 2)  # Blue color, thickness
 
+        #yellow_mask_colored = cv2.cvtColor(yellow_mask_roi, cv2.COLOR_GRAY2BGR)
+        #result_image = cv2.addWeighted(color_image_roi, 1, yellow_mask_colored, 0.5, 0)
+
         # Find contours in the foreground mask
 
-        contours, _ = cv2.findContours(foreground_mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+        #contours, _ = cv2.findContours(foreground_mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
         # New shit
         #color_image_roi = image.get_data()[roi]
@@ -127,26 +166,28 @@ while True:
            # for yellow_contour in yellow_contours[0]:
 
             # Find the contour with the maximum area
-        largest_contour = max(contours, key=cv2.contourArea)
+        #largest_contour = max(contours, key=cv2.contourArea)
 
-        if min_contour_area < cv2.contourArea(largest_contour) < max_contour_area:
+        #frame_with_contours = np.zeros_like(image_roi, dtype=np.uint8)
+
+        # Draw only the largest contour
+        #cv2.drawContours(frame_with_contours, [largest_contour], -1, (0, 255, 0), 2)  # Green color, thickness 2
+
+        #if min_contour_area < cv2.contourArea(largest_contour) < max_contour_area:
             # Create an empty black image with the same size as the RGB image
-            frame_with_contours = np.zeros_like(image_roi, dtype=np.uint8)
-
-            # Draw only the largest contour
-            cv2.drawContours(frame_with_contours, [largest_contour], -1, (0, 255, 0), 2)  # Green color, thickness 2
 
 
 
-        else:
+
+        #else:
             # If the largest contour does not meet the area thresholds, create an empty black image
-            frame_with_contours = np.zeros_like(image_roi, dtype=np.uint8)
+        #    frame_with_contours = np.zeros_like(image_roi, dtype=np.uint8)
 
             # Create an empty black image with the same size as the RGB image
         #frame_with_contours = np.zeros_like(image.get_data(), dtype=np.uint8)
 
             # Draw only the largest contour
-        cv2.drawContours(frame_with_contours, [largest_contour], -1, (0, 255, 0), 2)  # Green color, thickness 2
+        #cv2.drawContours(frame_with_contours, [largest_contour], -1, (0, 255, 0), 2)  # Green color, thickness 2
 
 
 
@@ -162,17 +203,20 @@ while True:
         # Display the segmented depth image
         # cv2.imshow("Segmented Depth", depth_image)
 
+        #cv2.imshow("Result", result_image)
 
         #yellow image
         cv2.imshow("Segmented RGB with Tracked Yellow Objects", color_image)
 
         # Display the RGB image with the largest contour
         #Black with green outline
-        cv2.imshow("Segmented RGB with Largest Contour", frame_with_contours)
+        #cv2.imshow("Segmented RGB with Largest Contour", frame_with_contours)
+
+        cv2.imshow("YELLOW result", yellow_mask)
 
         # Display the binary mask for visualization
         #Black and white
-        cv2.imshow("Foreground Mask", (foreground_mask * 255).astype('uint8'))
+        #cv2.imshow("Foreground Mask", (foreground_mask * 255).astype('uint8'))
 
         #full image
         #cv2.imshow("Segmented RGB with Largest Contour and Tracked Yellow Labels", image.get_data())
